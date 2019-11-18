3123302ba3df76b124743b8c6a252dd49fb6bd54
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-18 23:23:02 +0530

    created get endpoint for items

--- router/items.js
+++ router/items.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const itemsDb = require('../model/items');
 
 router.get('/', (req, res) => {
-    res.status(200).send(itemsDb.getMany());
+    itemsDb.getMany()
+        .then(items => {
+            res.status(200).json(items);
+        })
+        .catch(err => res.send(err));
 });
 
 module.exports = router;
