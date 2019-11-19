d6de09f1af88bbef6b8152b91ee8d74d89d92363
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-19 16:53:02 +0530

    moved dotenv from dev to prod

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const server = require('./api/server');
 require('dotenv').config();
+const server = require('./api/server');
 
+const port = process.env.PORT;
 
-const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`\nAPI running on port: ${port}\n`));
