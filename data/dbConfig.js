1bc170a932615badfb30350751904aa4e66d6d54
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-19 18:13:14 +0530

    using environment variable for database calls

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const knex = require('knex');
 const config = require('../knexfile');
 
 
-const database = knex(config['development'])
+const database = knex(config[process.env.DB_ENV])
 
 module.exports = database;
