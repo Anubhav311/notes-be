345d843f2b8f23379259a42bfc119056ee7f6fa9
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-19 18:06:01 +0530

    changed knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: process.env.DB_URL,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
