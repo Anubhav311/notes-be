91fc17db14b2249e4d90a6109e0342a7f860aa1c
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-20 16:00:12 +0530

    added getOne function to users model

--- model/users.js
+++ model/users.js
@@ -1,9 +1,14 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
+    getOne,
     getMany
 }
 
 function getMany(filter = {}) {
     return db('users').where(filter)
+}
+
+function getOne(filter = {}) {
+    return db('users').where(filter).first()
 }
