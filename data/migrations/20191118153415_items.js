c3824575cbb7a212bd6a7d215295467c4e795c8e
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-20 15:16:01 +0530

    did some formatting to migrations file

--- data/migrations/20191118153415_items.js
+++ data/migrations/20191118153415_items.js
@@ -2,27 +2,65 @@
 exports.up = function(knex) {
     return knex.schema.createTable('users', tbl => {
         tbl.increments();
-        tbl.datetime('created_at').defaultTo(knex.fn.now());
+
+        tbl.datetime('created_at')
+            .defaultTo(knex.fn.now());
+        
         tbl.string('first_name');
+        
         tbl.string('last_name');
-        tbl.string('email').notNullable();
-        tbl.string('password').notNullable();
+        
+        tbl.string('email')
+            .notNullable();
+        
+        tbl.string('password')
+            .notNullable();
     })
     .createTable('skills', tbl => {
         tbl.increments();
-        tbl.datetime('created_at').defaultTo(knex.fn.now());
-        tbl.string('skill').notNullable();
+
+        tbl.datetime('created_at')
+            .defaultTo(knex.fn.now());
+
+        tbl.string('skill')
+            .notNullable();
+
         tbl.string('category')
-        tbl.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
+
+        tbl.integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
     })
     .createTable('skill_parts', tbl => {
         tbl.increments();
-        tbl.datetime('created_at');
-        tbl.string('part_name').notNullable();
-        tbl.boolean('completion_status').defaultTo(false).notNullable();
-        tbl.boolean('social_status').defaultTo(false).notNullable();
-        tbl.boolean('direct_or_indirect').defaultTo(false).notNullable();
-        tbl.integer('skill_id').notNullable().references('id').inTable('skills').onDelete('CASCADE').onUpdate('CASCADE');
+
+        tbl.datetime('created_at')
+            .defaultTo(knex.fn.now());
+
+        tbl.string('part_name')
+            .notNullable();
+
+        tbl.boolean('completion_status')
+            .defaultTo(false)
+            .notNullable();
+
+        tbl.boolean('social_status')
+            .defaultTo(false)
+            .notNullable();
+
+        tbl.boolean('direct_or_indirect')
+            .defaultTo(false)
+            .notNullable();
+
+        tbl.integer('skill_id')
+            .notNullable()
+            .references('id')
+            .inTable('skills')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
     })
 };
 
