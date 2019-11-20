bcdcdda500be660814edb157015bd4fbb7fd5307
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-20 15:37:51 +0530

    changed variable names and file names in server

--- api/server.js
+++ api/server.js
@@ -4,15 +4,15 @@ const cors = require('cors');
 
 const server = express();
 
-const itemsRouter = require('../router/items');
-const commentsRouter = require('../router/comments');
+const usersRouter = require('../router/users');
+const skillsRouter = require('../router/skills');
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json())
 
-server.use('/api/items', itemsRouter);
-server.use('/api/comments', commentsRouter);
+server.use('/api/users', usersRouter);
+server.use('/api/skills', skillsRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json("it's working")
