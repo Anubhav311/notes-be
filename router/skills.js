7a59a9f6489749fd6971a0053d6ec7a0f84a2f94
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-23 01:58:10 +0530

    created put endpoint for skills table

--- router/skills.js
+++ router/skills.js
@@ -11,4 +11,12 @@ router.get('/', (req, res) => {
         .catch(err => res.send(err));
 });
 
+router.put('/', (req, res) => {
+    skillsDb.update({id: req.body.id}, req.body.payload)
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => res.send(err));
+})
+
 module.exports = router;
