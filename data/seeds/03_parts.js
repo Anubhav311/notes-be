cac851e03228b3d650b5c4ad76e91c6ee3058eb3
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-23 02:23:36 +0530

    changed seeds for parts table

--- data/seeds/03_parts.js
+++ data/seeds/03_parts.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('skill_parts').del()
+  return knex('parts').del()
     .then(function () {
       // Inserts seed entries
-      return knex('skill_parts').insert([
+      return knex('parts').insert([
         {part_name: "test part name", skill_id: 1},
         {part_name: "test part name", skill_id: 2},
         {part_name: "test part name", skill_id: 3}
