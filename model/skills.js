615b82ea8d4e8b9a6001d79b4aa2b0d461d14948
Anubhav311 <anubhavbhambri1@gmail.com>
2019-11-23 01:55:17 +0530

    created update model for skills table

--- model/skills.js
+++ model/skills.js
@@ -2,7 +2,8 @@ const db = require('../data/dbConfig');
 
 module.exports = {
     insert,
-    getMany
+    getMany,
+    update
 }
 
 function insert(user) {
@@ -18,4 +19,18 @@ function insert(user) {
 
 function getMany(filter = {}) {
     return db('skills').where(filter)
+}
+
+function update(filter = {}, payload) {
+    console.log(filter, payload)
+    return db('skills')
+        .where(filter)
+        .update(payload)
+        .returning('id')
+        .then(idArr => {
+            const id = idArr[0]
+            return db('skills')
+                .where({id})
+                .first();
+        })
 }
